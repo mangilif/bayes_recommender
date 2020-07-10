c6740e8eb39501a2386606feb26420b99c20453c
mangilif <francesca@idsia.ch>
2020-07-10 09:31:05 +0200

    Add files via upload

---
+++ adaptive_movieLens.py
@@ -0,0 +1,257 @@
+import numpy as np
+import pandas as pd
+import pickle
+import random as rn
+import time
+
+
+
+def updating(prior, tables, q, a):
+    likelihood = tables[q][:, a]
+    joint = likelihood*prior
+    posterior = joint / sum(joint)
+    return posterior
+	
+def entropy(mass_function):
+	
+	base = len(mass_function)
+	np.place(mass_function, mass_function==0, 1)
+	return (-mass_function*np.log(mass_function)).sum(0)/np.log(base)
+
+def pick_question(prior, tables, askable_questions):
+	
+	entropies = [2 for _ in tables] # Array of the conditional entropies H(I|Q) for each question Q
+	priort = prior.reshape(-1,1)
+	for question in askable_questions: # Only allowed questions are considered as candidates
+
+		conditional_table = tables[question].copy()
+		joint_table = conditional_table*np.repeat(priort,conditional_table.shape[1], axis = 1)
+		
+		# Compute P(Q)
+		question_probabilities = joint_table.sum(axis=0)/joint_table.sum() # P(Q)
+		
+		if (joint_table.sum(0)==0).any():
+			posterior = joint_table
+			posterior[:,joint_table.sum(0)==0] = priort
+			posterior = posterior/posterior.sum(0)
+		else: 
+			posterior = joint_table/joint_table.sum(0)
+            
+		# Compute H(I|Q)
+		entropies[question] = (entropy(posterior)*question_probabilities).sum()
+	entropies = [_ if _ >= 0.0 else 2.0 for _ in entropies]
+	return np.argmin([_ for _ in entropies if _ >= 0])
+
+
+# MAIN 
+# Select dataset 
+# 10:= artificial dataset with noise level at 10%
+# 50:= artificial dataset with noise level at 50%
+noise = 10
+
+resultspath = '/results/noise%d/'%noise
+user_features = pd.read_pickle('data/user_features_noise_at_'+str(noise)+'_percent_small_test.pkl')
+pQgivenIdict = pickle.load( open('data/allcpts'+str(noise)+'_small.pkl', "rb" ) )
+
+item_features = pd.read_pickle('data/item_features_small.pkl')
+n_items = pQgivenIdict['genre'].shape[0]                                  # Number of items in the catalogue
+
+def L1distance(a, catalogue):
+    if type(a)==np.ndarray:
+        return (catalogue - a).apply(abs).apply(sum)
+    else:
+        return (catalogue-a).abs()  
+
+for i in range(pQgivenIdict['genre'].shape[1]): 
+    pQgivenIdict['genre_'+str(i)] = np.array([1-pQgivenIdict['genre'][:,i]/7,pQgivenIdict['genre'][:,i]/7]).transpose()
+    item_features['genre_'+str(i)] = item_features['genre'].apply(lambda x: x[i])
+
+del pQgivenIdict['genre']    
+del item_features['genre']  
+
+n_answers = list()  # Number of possible answers for each question  
+
+for q,cpt in pQgivenIdict.items():
+    n_answers += [cpt.shape[1]]                   # Number of possible answers for each question
+n_questions = len(n_answers)
+
+cols = ['p'+str(_) for _ in item_features.index]+['question', 'answer']
+
+nusers = user_features.shape[0]
+emptyDF = pd.DataFrame(columns = range(nusers))
+methods = ['random', 'adaptive', 'similarity', 'similarity_random']
+rtable = dict()
+ptable = dict()
+
+for m in methods:
+    rtable[m] = emptyDF.copy()
+    ptable[m] = emptyDF.copy()
+
+
+for i in range(nusers):
+    
+    answers = user_features.iloc[i].copy()
+    # transform list of genres to 19 different questions 
+    for g in range(len(answers['genre'])): 
+        answers['genre_'+str(g)] = answers['genre'][g]
+    answers = answers.drop(index = 'genre')
+    valid_answers = answers.dropna().index
+    trueI = user_features.iloc[i].name
+    col = "p"+str(trueI)
+    print('Item:', trueI)
+    answers.loc[valid_answers] = answers.loc[valid_answers].astype(int) 
+
+    tables = list(pQgivenIdict.values())
+    
+################################################################################    
+# Adaptive
+    method = 'adaptive'     
+    # Uniform Prior
+    prior = np.array([1.0/n_items for _ in range(n_items)])  # P(I)
+    df = pd.DataFrame(columns=cols+['entropy'])
+    df.loc[0] = np.append(prior, [-1, -1, entropy(prior.copy())])
+    # All the questions can be initially asked
+    askable_questions = [_ for _ in range(200)]#range(n_questions)]
+   
+    q = 0
+    start0 = time.time()
+    while(len(askable_questions)>0):
+     	# Pick the most informative question and remove from the candidates list
+     	question_asked =  pick_question(prior, tables, askable_questions)
+     	askable_questions.remove(question_asked)
+     	if np.isnan(answers[question_asked]): continue
+     	# Update prior (to the posterior)
+     	prior = updating(prior, tables, question_asked, answers[question_asked])
+    	
+     	# Add information to the df
+     	df.loc[len(df)] = np.append(prior, [question_asked, answers[question_asked], entropy(prior.copy())])
+     	q+=1
+    end = time.time()    
+   
+    rtable[method][i] = df.drop(columns = ['question', 'answer', 'entropy']).rank(axis=1,ascending=False)[col]
+    ptable[method][i] = df[col]
+    print('Final rank BN: ', rtable[method][i].iloc[-1] )
+    print('Final probability BN: ', ptable[method][i].iloc[-1])
+
+#############################################33
+# Similarity 
+    method = 'similarity'
+    distance =  pd.Series(0, index = item_features.index)        
+    prior = np.array([1.0/n_items]*n_items)  # P(I)
+    dfsim = pd.DataFrame(columns=cols+['correct'])
+    dfsim.loc[0] = np.append(prior, [-1, -1,-1])
+    AB = 1
+    A2 = 1
+    B2 = 1
+    for q in df.question[1:].astype(int):
+        answer =answers.iloc[q]
+        if np.isnan(answer): continue
+        items = item_features.iloc[:,q]
+        if np.isscalar(answer):
+            AB += (answer*items)
+            A2 += answer**2 
+            B2 += items**2
+        else:
+            answer = user_features.iloc[[i],q]
+            AB += items.apply(lambda x: answer.values[0]*x).apply(sum)
+            A2 += (answer.values[0]**2).sum()
+            B2 += (items**2).apply(sum)
+        
+        similarity =AB/np.sqrt(A2*B2)
+        distance +=L1distance(answer,items)
+        prior = similarity
+    	
+    	# Add information to the df
+        dfsim.loc[len(dfsim)] = np.append(prior, [q, answers[q], items[trueI]])
+    rtable[method][i] = dfsim.drop(columns = ['question', 'answer']).rank(axis=1,ascending=False)[col]
+    ptable[method][i] = dfsim[col]
+
+    print('Final rank similarity: ',  rtable[method][i].iloc[-1])
+    print('Final similarity: ',  ptable[method][i].iloc[-1])
+    print('Time adaptive: ', end - start0)	  
+
+################################################################################
+
+# Random
+    method = 'random'
+    # Uniform Prior
+    prior = np.array([1.0/n_items for _ in range(n_items)])  # P(I)
+    df = pd.DataFrame(columns=cols+['entropy'])
+    df.loc[0] = np.append(prior, [-1, -1, entropy(prior.copy())])
+    # All the questions can be initially asked
+    askable_questions = [_ for _ in range(200)]#range(n_questions)]
+    q = 0
+    start0 = time.time()
+    while(len(askable_questions)>0):
+#    	start = time.time()
+     	# Pick a random question and remove from the candidates list
+     	question_asked = rn.choice(askable_questions)
+     	askable_questions.remove(question_asked)
+     	if np.isnan(answers[question_asked]): continue
+     	# Update prior (to the posterior)
+     	prior = updating(prior, tables, question_asked, answers[question_asked])
+    	
+     	# Add information to the df
+     	df.loc[len(df)] = np.append(prior, [question_asked, answers[question_asked], entropy(prior.copy())])
+     	q+=1
+    end = time.time()    
+    print('Time random: ', end - start0)	
+    
+    rtable[method][i] = df.drop(columns = ['question', 'answer', 'entropy']).rank(axis=1,ascending=False)[col]
+    ptable[method][i] = df[col]  
+  
+    
+#############################################33
+# Similarity random
+   
+    method = 'similarity_random'    
+    prior = np.array([1.0/n_items]*n_items)  # P(I)
+    dfsimrnd = pd.DataFrame(columns=cols)
+    dfsimrnd.loc[0] = np.append(prior, [-1, -1])
+    AB = 1
+    A2 = 1
+    B2 = 1
+
+    for q in df.question[1:].astype(int):
+        answer =answers.iloc[q]
+        items = item_features.iloc[:,q]
+        if np.isnan(answer): continue
+        if np.isscalar(answer):
+            AB += (answer*items)
+            A2 += answer**2 
+            B2 += items**2
+        else:
+            answer = user_features.iloc[[i],q]
+            AB += items.apply(lambda x: answer.values[0]*x).apply(sum)
+            A2 += (answer.values[0]**2).sum()
+            B2 += (items**2).apply(sum)
+        
+        similarity =AB/np.sqrt(A2*B2)
+        prior = similarity
+    	
+    	# Add information to the df
+        dfsimrnd.loc[len(dfsimrnd)] = np.append(prior, [q, answers[q]])
+
+    rtable[method][i] = dfsimrnd.drop(columns = ['question', 'answer']).rank(axis=1,ascending=False)[col]
+    ptable[method][i] = dfsimrnd[col] 
+        
+
+f = open('results/ranks%s_small.pkl'%str(noise),"wb")
+pickle.dump(rtable,f)
+f.close()
+f = open('results/probabilities%s_small.pkl'%str(noise),"wb")
+pickle.dump(ptable,f)
+f.close()
+
+probabilities = pd.DataFrame()
+ranks = pd.DataFrame()
+for m in methods:
+    probabilities[m] = ptable[m].mean(1)
+    ranks[m] = rtable[m].mean(1)
+
+probabilities[['random', 'adaptive']].plot()
+ranks.loc[1:,].plot()
+ranks.loc[150:,].plot()
+probabilities.to_csv('results/probabilities%s.csv'%str(noise))
+ranks.to_csv('results/ranks%s.csv'%str(noise))
+
---
+++ learnCPTs.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 10:13:38 2020
+
+@author: francesca
+"""
+
+import os
+import pandas as pd
+import numpy as np
+import pickle
+from scipy.optimize import LinearConstraint, minimize
+ 
+path =os.getcwd()
+datapath = path + '/data/'
+
+
+# Select dataset 
+# 10:= artificial dataset with noise level at 10%
+# 50:= artificial dataset with noise level at 50%
+noise = 10
+name = 'user_features_noise_at_%d_percent'%noise
+outfilename = 'allcpts%d_small.pkl'%noise
+
+#%% Reduce dataset
+tags = pd.read_pickle(datapath+'small_dataset_tags.pkl')
+movies = pd.read_pickle(datapath+'small_dataset_movies.pkl')
+
+user_features = pd.read_pickle(datapath+name+'.pkl')
+user_features_test_small = user_features.loc[[m for m in movies if m in user_features.index],['tag_'+str(t) for t in tags if 'tag_'+str(t) in user_features.columns]+['genre','period']]
+user_features_train_small = user_features.loc[~user_features.index.isin(movies),['tag_'+str(t) for t in tags if 'tag_'+str(t) in user_features.columns]+['genre','period']]
+user_features_test_small.to_pickle(datapath+name+'_small_test.pkl')
+
+item_features = pd.read_pickle(datapath+'item_features.pkl')
+item_features_small = item_features.loc[movies,[t for t in user_features_test_small.columns]]
+item_features_small.to_pickle(datapath+'item_features_small.pkl')
+
+#%%
+item_features = item_features
+user_features = user_features_train_small
+
+pCgivenIdict = dict()
+
+nitems = item_features.shape[0]
+for q in item_features.columns:
+    feat = item_features[q]
+    if q=='period':
+        answers = set(feat)
+        pCgivenIdict[q] = pd.get_dummies(feat)
+    elif item_features[q].dtypes=='object':
+        answers = len(item_features[q].iloc[0])
+        pCgivenIdict[q] = item_features[q].apply(lambda x: pd.Series(x,index = range(answers)))
+    elif item_features[q].dtypes=='float':
+        cpt = pd.DataFrame([1-feat,feat]).transpose()  
+        pCgivenIdict[q] = cpt
+    else:
+        print('problem with ',q )
+    
+f = open(datapath+'pCgivenIdict.pkl',"wb")
+pickle.dump(pCgivenIdict,f)
+f.close()
+
+pCgivenIdict_small = dict()
+for f in ['tag_'+str(t) for t in tags]+['genre','period']:
+    pCgivenIdict_small[f] = pCgivenIdict[f].loc[movies,:].values
+
+s = 1
+
+def assign_counts(x,I,pCgivenI):
+    C = pCgivenI.loc[I]
+    Ca = C
+    nx = np.sum(x)
+    counts = np.array([[0.]*len(x)]*len(x))
+    if (x*C).sum()>0:
+        counts = counts + np.diag(x*C/nx)
+        Ca = Ca - x*C
+        x = x-x*C
+        
+    if (x*(1-C)).sum()>0:
+        for i in np.where(x*(1-C)!=0)[0]:
+            counts[i,np.where(Ca)[0]] = 1/(nx*Ca.sum())
+    if counts.sum()<0.999:
+        if x.sum()>0:
+            print('Wrong counts for item', I)
+            return
+    return counts
+
+def PQgivenC(collected_answers, pCgivenI, qtype):
+    
+    # multiple properties and multiple answers
+    if qtype=='genre':
+        na = len(collected_answers.iloc[0])
+        counts = np.eye(na)*s
+        for i,r  in collected_answers.iteritems():
+            counts += assign_counts(r,i,pCgivenI)
+        #check
+        nonempty_answers = (collected_answers.apply(lambda x: np.sum(x))!=0).sum()
+        if np.abs(nonempty_answers+na*s-counts.sum())>0.1: print('warning')
+     
+    # single property and single answers
+    elif qtype=='period':
+        nA = pCgivenI.shape[1]
+        I = collected_answers.index.values
+        pC = pCgivenI.loc[I,:]
+        counts = np.array([[np.nan]*nA]*nA)
+        for a in range(nA):
+            counts[a,:] = pC.apply(lambda x: x*(collected_answers==a)).sum()
+        counts = counts+np.eye(nA)*s    
+        nonempty_answers = collected_answers.dropna().count()
+        if np.abs(nonempty_answers+nA*s-counts.sum())>0.1: print('warning',q)
+        
+   
+    elif qtype=='tag':
+        nA = 2
+        I = collected_answers.index.values
+        pC = pCgivenI.loc[I,:]
+        counts = np.array([[np.nan]*nA]*nA)
+        lc = LinearConstraint(np.eye(2),np.zeros(2), np.ones(2))
+        # c = minimize(lambda x: -likelihood(x,pC), 0.5,constraints=lc, method = 'trust-constr').x[0]
+        c = minimize(lambda x: -likelihood(x,pC), [0,1],constraints=lc, method = 'trust-constr').x
+        print(c)
+        counts[0,0] = c[1]
+        counts[1,0] = 1-c[1]
+        counts[0,1] = c[0]
+        counts[1,1] = 1-c[0]
+   
+        nonempty_answers = collected_answers.dropna().count()
+         
+    return counts.transpose()
+
+
+def likelihood(x,pC):
+    if (x>=1).any()|(x<=0).any():
+        like = -1e20
+    else:
+        like = (-np.log(x[0]) +(s-1)*np.log(x[1]) 
+                + (s-1)*np.log(1-x[0]) -np.log(1-x[1])
+                +np.sum(np.log(x[0]*pC.iloc[:,1]+x[1]*pC.iloc[:,0])*(1-collected_answers))
+                +np.sum(np.log((1-x[0])*pC.iloc[:,1]+(1-x[1])*pC.iloc[:,0])*(collected_answers)))
+    return like
+
+counts = dict() #Tables of counts: answers are on v, properties on rows
+pQgivenCdict = dict() #Tables of P(Q|C): answers are on columns, properties on rows
+pQgivenIdict = dict() #Tables of P(Q|I): answers are on columns, items on rows
+for q in user_features.columns:
+    collected_answers = user_features[q]
+    pCgivenI = pCgivenIdict[q]
+    qtype = q.split('_')[0]
+    counts[q] = PQgivenC(collected_answers, pCgivenI,qtype)
+    
+    pQgivenCdict[q] = (counts[q]/counts[q].sum(1).reshape([-1,1])) 
+    pQgivenIdict[q] = np.dot(pCgivenIdict_small[q],pQgivenCdict[q])        
+
+f = open(datapath+outfilename,"wb")
+pickle.dump(pQgivenIdict,f)
+f.close()
